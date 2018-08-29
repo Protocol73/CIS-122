57c9db6745bf509eafb951cbc0cde7dc955951c7
Protocol73 <Protocol73@bellevuelabs@net>
2018-08-29 14:05:11 -0500

    earned
    still needs a quit command

--- hourly-pay.py
+++ hourly-pay.py
@@ -1,6 +1,23 @@
-hourly-pay.py # -Protocol73
+# -Protocol73
+import os
+import sys
+def cls(): #clear the Screen
+    os.system('cls' if os.name=='nt' else 'clear')
+cls() 
 
-HrsWorked = raw_input("Hours Worked")
-HrlyWage = raw_input("Hourly Wage")
-GrossWage = 
-TAX_RATE = 0.2
+HrsWorked = raw_input("Hours Worked:")
+HrlyWage = raw_input("Hourly Wage:")
+
+'''
+if HrsWorked in ['q' or 'quit']:
+     print("Goodbye")
+     time.sleep(1)
+     sys.exit()
+'''
+
+TAX_RATE = (0.20)
+GrossWage =  int(HrsWorked) * float(HrlyWage)
+TaxesOnEarned = GrossWage * TAX_RATE
+earned = GrossWage -TaxesOnEarned
+
+print("Yor Made: $" + str(earned))
