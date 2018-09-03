2802a3af601c3995739744a53dd1cce888f2c1c4
Protocol73 <Protocol73@BellevueLabs.net>
2018-09-03 16:08:13 -0500

    A few touch ups
    Added .gitignore & cleaned up the code a bit

--- add&diff.py
+++ add&diff.py
@@ -1,16 +1,14 @@
 #add&diff.py -Protocol73
 import os
-def cls(): #clear the Screen
+def clear_term(): #clear the Screen
     os.system('cls' if os.name=='nt' else 'clear')
-cls() 
+clear_term() 
 
 number1 = raw_input("Input a Number:")
 number2 = raw_input("Put in another Number:")
-
 added = int(number1) + int(number2)
 diff = int(number1) - int(number2)
 
-diff_absolute = abs(diff)
-
+clear_term()
 print ("Your Numbers added are:") + (str(added))
-print ("And the Difference is:") + (str(diff_absolute))
+print ("And the Difference is:") + (str(abs(diff)))
--- hourly-pay.py
+++ hourly-pay.py
@@ -3,25 +3,25 @@ import os
 import sys
 import time
 
-def cls(): #clear the Screen
+def clear_term(): #clear the Screen
     os.system('cls' if os.name=='nt' else 'clear')
-cls() 
+clear_term() 
+
 print("Calulate your wages earned after 20% Taxes \nEnter a Negative Value to Quit.")
+
 HrsWorked = raw_input("Hours Worked:")
 
-
-#for number in HrsWorked:
 if float(HrsWorked) < 1:
-	cls()
+	clear_term()
 	print ("Goodbye")
 	time.sleep(2)
-	cls()
+	clear_term()
 	sys.exit()
 
 HrlyWage = raw_input("Hourly Wage:")
 TAX_RATE = (0.20)
 GrossWage =  int(HrsWorked) * float(HrlyWage)
 TaxesOnEarned = GrossWage * TAX_RATE
-earned = GrossWage -TaxesOnEarned
+earned = GrossWage - TaxesOnEarned
 
 print("You Made: $" + str(earned))
