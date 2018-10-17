ecff0f3a88e783fc8b8875f277f92e37b42c83bc
Matthew Law <matthew.law@bellevuelabs.net>
2018-10-17 14:45:30 -0500

    add #223

---
+++ 223.py
@@ -0,0 +1,21 @@
+j = 2
+k = 5
+n = 9
+
+
+print("j",j)
+print("k",k)
+print("n",n)
+
+while j < k:
+	m = 6
+	while m < n:
+		print("M",m)
+		print("Goodbye")
+		m = m + 1
+	j = j + 1
+
+
+print("j",j)
+print("k",k)
+print("n",n)
